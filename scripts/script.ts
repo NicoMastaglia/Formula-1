abcfc3f3fc91bfb30f81dda1a1914299fab01d6f
NicoMastaglia <nicomastaglia@gmail.com>
2026-07-31 16:30:46 +0200

    Migrazione javascript -> typescript

---
+++ scripts/script.ts
@@ -0,0 +1,382 @@
+const MAIN = document.getElementById("main") as HTMLDivElement;
+const standingBtn = document.getElementById(
+  "standing-btn",
+) as HTMLButtonElement;
+const resultBtn = document.getElementById("result-btn") as HTMLButtonElement;
+const buttonBg = document.getElementById("test") as HTMLDivElement;
+const nationalityMapping: Record<string, string> = {
+  British: "United Kingdom",
+  Monegasque: "Monaco",
+  Dutch: "Netherlands",
+  Swiss: "Switzerland",
+  American: "United States",
+  French: "France",
+  German: "Germany",
+  Spanish: "Spain",
+  Portuguese: "Portugal",
+  Italian: "Italy",
+  Japanese: "Japan",
+  Chinese: "China",
+  Argentine: "Argentina",
+  Argentinian: "Argentina",
+  Austrian: "Austria",
+  Belgian: "Belgium",
+  Brazilian: "Brazil",
+  Canadian: "Canada",
+  Danish: "Denmark",
+  Egyptian: "Egypt",
+  Finnish: "Finland",
+  Greek: "Greece",
+  Icelandic: "Iceland",
+  Irish: "Ireland",
+  Mexican: "Mexico",
+  Norwegian: "Norway",
+  Polish: "Poland",
+  Russian: "Russia",
+  Swedish: "Sweden",
+  Turkish: "Turkey",
+  Thai: "Thailand",
+  Vietnamese: "Vietnam",
+  Australian: "Australia",
+  Newzealander: "New Zealand",
+  Emirati: "United Arab Emirates",
+  Saudi: "Saudi Arabia",
+  Korean: "South Korea",
+  Indian: "India",
+  Pakistani: "Pakistan",
+};
+
+function refresh() {
+  if (standingBtn.classList[1] == "not-select") {
+    loadResultPage();
+  } else {
+    loadStandingPage();
+  }
+}
+
+async function loadStandingPage() {
+  standingBtn.classList.add("select");
+  standingBtn.classList.remove("not-select");
+  resultBtn.classList.add("not-select");
+  resultBtn.classList.remove("select");
+  buttonBg.style.right = "60%";
+
+  MAIN.innerHTML = "";
+  const content = `
+  <div class="section" id="constructor-standing">
+  <p class="section-title">Classifica costruttori</p>
+  <div class="information">
+        <span id="team">Team</span>
+        <span>Vittorie</span>
+        <span>Punti</span>
+        </div>
+        <div id="constructor-standing-data"></div>
+        </div>
+        
+        <div class="section" id="driver-standing">
+        <p class="section-title">Classifica piloti</p>
+        <div class="driver-information">
+        <span id="driver">Pilota</span>
+        <span id="team">Team</span>
+        <span>Vittorie</span>
+        <span>Punti</span>
+        </div>
+        <div id="driver-standing-data"></div>
+        </div>
+        `;
+  MAIN.innerHTML = content;
+
+  printConstructorStanding(await getConstructorsStanding());
+  printDriverStanding(await GetDriverStanding());
+}
+
+async function loadResultPage() {
+  resultBtn.classList.add("select");
+  resultBtn.classList.remove("not-select");
+  standingBtn.classList.add("not-select");
+  standingBtn.classList.remove("select");
+  buttonBg.style.right = "20%";
+
+  MAIN.innerHTML = "";
+  const content = `
+      <div class="race-box">
+        <div class="race-list"></div>
+        <div class="race-result"></div>
+      </div>
+  `;
+  MAIN.innerHTML = content;
+
+  printRaceList(await getRaceList());
+  printRaceResult(await getRaceResult(1));
+
+  const raceList = document.querySelector(".race-list") as HTMLDivElement;
+  raceList.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    raceList.scrollLeft += e.deltaY;
+  });
+}
+
+function checkYear(year: number) {
+  const currentYear = new Date().getFullYear();
+
+  if (year >= 1950 && year <= currentYear) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function getConstructorsStanding() {
+  const year = parseInt(
+    (document.getElementById("year") as HTMLInputElement).value,
+  );
+  const url = `https://api.jolpi.ca/ergast/f1/${year}/constructorstandings/`;
+  if (checkYear(year)) {
+    const response = await fetch(url);
+    if (!response.ok) {
+      return "Errore nel caricamento dei dati";
+    }
+
+    const result = await response.json();
+    if (result.MRData.StandingsTable.StandingsLists.length == 0) {
+      return "Nessun risultato rilevato in questo anno";
+    } else {
+      return result.MRData.StandingsTable.StandingsLists[0]
+        .ConstructorStandings;
+    }
+  } else {
+    return "Nessun risultato rilevato in questo anno";
+  }
+}
+
+async function GetDriverStanding() {
+  const year = parseInt(
+    (document.getElementById("year") as HTMLInputElement).value,
+  );
+  const url = `https://api.jolpi.ca/ergast/f1/${year}/driverstandings/`;
+  if (checkYear(year)) {
+    const response = await fetch(url);
+    if (!response.ok) {
+      return "Errore nel caricamento dei dati";
+    }
+
+    const result = await response.json();
+    if (result.MRData.StandingsTable.StandingsLists.length == 0) {
+      return "Nessun risultato rilevato in questo anno";
+    } else {
+      return result.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    }
+  } else {
+    return "Nessun risultato rilevato in questo anno";
+  }
+}
+
+async function getFlag(country: string) {
+  const normalizedInput =
+    country.charAt(0).toUpperCase() + country.slice(1).toLowerCase();
+  const countryName = nationalityMapping[normalizedInput] || normalizedInput;
+
+  const url = `https://restcountries.com/v3.1/name/${countryName}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    return "https://flagcdn.com/w320/white.png";
+  }
+
+  const result = await response.json();
+  return result[0]?.flags?.png || "https://flagcdn.com/w320/white.png";
+}
+
+async function printConstructorStanding(data: any) {
+  const constructorStanding = document.getElementById(
+    "constructor-standing-data",
+  ) as HTMLDivElement;
+  constructorStanding.textContent = "";
+
+  if (typeof data == "string") {
+    let newLine = `<p>
+    <span class="team-name" style="width: 100%">${data}</span>
+    </p>`;
+    constructorStanding.innerHTML += newLine;
+  } else {
+    for (let i = 0; i < data.length; i++) {
+      let flagUrl = await getFlag(
+        data[i].Constructor.nationality.replace(/\s+/g, ""),
+      );
+
+      let newLine = `<p>
+      <span class="team-name"><img src="${flagUrl}" class="flag-img"> ${i + 1}. ${data[i].Constructor.name}</span>
+      <span class="team-wins">${data[i].wins}</span>
+      <span class="team-point">${data[i].points}</span>
+      </p>`;
+      constructorStanding.innerHTML += newLine;
+    }
+  }
+}
+
+async function printDriverStanding(data: any) {
+  const driverrStanding = document.getElementById(
+    "driver-standing-data",
+  ) as HTMLDivElement;
+  driverrStanding.textContent = "";
+
+  if (typeof data == "string") {
+    let newLine = `<p>
+    <span class="team-name" style="width: 100%">${data}</span>
+    </p>`;
+    driverrStanding.innerHTML += newLine;
+  } else {
+    for (let i = 0; i < data.length; i++) {
+      let flagUrl = await getFlag(
+        data[i].Driver.nationality.replace(/\s+/g, ""),
+      );
+
+      let newLine = `<p>
+      <span class="team-name">
+      <img src="${flagUrl}" class="flag-img"> 
+      ${i + 1}. 
+      ${data[i].Driver.familyName}
+      </span>
+      <span class="driver-team">${data[i].Constructors[data[i].Constructors.length - 1].name}</span>
+      <span class="team-wins">${data[i].wins}</span>
+      <span class="team-point">${data[i].points}</span>
+      </p>`;
+      driverrStanding.innerHTML += newLine;
+    }
+  }
+}
+
+async function getRaceList() {
+  const year = parseInt(
+    (document.getElementById("year") as HTMLInputElement).value,
+  );
+  const url = `https://api.jolpi.ca/ergast/f1/${year}/`;
+
+  if (checkYear(year)) {
+    const response = await fetch(url);
+    if (!response.ok) {
+      return "Errore nel caricamento dei dati";
+    }
+
+    const result = await response.json();
+    if (result.MRData.RaceTable.Races.length != 0) {
+      return result.MRData.RaceTable.Races;
+    } else {
+      return "Nessun risultato rilevato in questo anno";
+    }
+  } else {
+    return "Nessun risultato rilevato in questo anno";
+  }
+}
+
+async function printRaceList(data: any) {
+  const raceContainer = document.querySelector(".race-list") as HTMLDivElement;
+  if (typeof data != "string") {
+    raceContainer.innerHTML += "<div class='cover'></div>";
+    for (let i = 0; i < data.length; i++) {
+      let newLine = `
+        <span 
+          id="round-${data[i].round}" 
+          class="${i == 0 ? "active" : "not-active"}"
+          onclick="(changeRace('round-${data[i].round}'), printRaceResult(getRaceResult('${data[i].round}')))">
+            ${data[i].Circuit.Location.country}
+        </span>
+      `;
+      raceContainer.innerHTML += newLine;
+    }
+  }
+}
+
+function changeRace(id: string) {
+  const idSplit = parseInt(id.split("-")[1]);
+  const raceContainer = document.querySelector(".race-list") as HTMLDivElement;
+  const cover = document.querySelector(".cover") as HTMLDivElement;
+
+  for (let i = 0; i < raceContainer.children.length; i++) {
+    raceContainer.children[i].classList.remove("active");
+    raceContainer.children[i].classList.add("not-active");
+  }
+  cover.style.left = `${(idSplit - 1) * 15}%`;
+  raceContainer.children[idSplit].classList.add("active");
+  raceContainer.children[idSplit].classList.remove("not-active");
+}
+
+async function getRaceResult(round: string | number) {
+  const year = parseInt(
+    (document.getElementById("year") as HTMLInputElement).value,
+  );
+  const url = `https://api.jolpi.ca/ergast/f1/${year}/${round}/results`;
+
+  if (checkYear(year)) {
+    const response = await fetch(url);
+    if (!response.ok) {
+      return "Errore nel caricamento dei dati";
+    }
+
+    const result = await response.json();
+    if (result.MRData.RaceTable.Races.length == 0) {
+      return "Nessun dato disponibile per questa gara";
+    }
+
+    return result.MRData.RaceTable.Races;
+  } else {
+    return "Nessun risultato rilevato in questo anno";
+  }
+}
+
+async function printRaceResult(data: any) {
+  const loadData = await data;
+  console.log(loadData);
+  const BOX = document.querySelector(".race-result") as HTMLDivElement;
+  BOX.innerHTML = "";
+  if (typeof loadData == "string") {
+    BOX.innerHTML += `<p class="error-message">${loadData}</p>`;
+  } else {
+    BOX.innerHTML += `
+                    <div class="info-container">
+                      <p class="race-name">${loadData[0].raceName}</p>
+                      <div class="race-info">
+                        <span>Località:</span><span class="info-span">${loadData[0].Circuit.Location.locality}</span> 
+                        <span>Circuito:</span><span class="info-span">${loadData[0].Circuit.circuitName}</span>        
+                        <span>Data:</span><span class="info-span">${loadData[0].date}</span>
+                        <span>Round:</span><span class="info-span">${loadData[0].round}</span>
+                      </div>
+                    </div>
+                    <div class="race-standing">
+                      <p class="result-info">
+                        <span id="result-position">Pos</span>
+                        <span id="result-driver">Pilota</span>
+                        <span id="result-team">Team</span>
+                        <span id="result-time">Tempo</span>
+                      </p>
+                    </div>
+                    `;
+
+    const DBOX = document.querySelector(".race-standing") as HTMLDivElement;
+    for (let i = 0; i < loadData[0].Results.length; i++) {
+      let flagUrl = await getFlag(
+        loadData[0].Results[i].Driver.nationality.replace(/\s+/g, ""),
+      );
+
+      DBOX.innerHTML += `
+                      <p id="driver-container">
+                        <span id="result-position">${loadData[0].Results[i].position}</span> 
+                        <span id="result-driver"><img src="${flagUrl}"> ${loadData[0].Results[i].Driver.givenName} ${loadData[0].Results[i].Driver.familyName}</span>        
+                        <span id="result-team">${loadData[0].Results[i].Constructor.name}</span>
+                        <span id="result-time">
+                          ${loadData[0].Results[i].status == "Finished" ? loadData[0].Results[i].Time?.time : ""}
+                          ${loadData[0].Results[i].status == "Lapped" ? (loadData[0].Results[0].laps - loadData[0].Results[i].laps == 1 ? "+" + (loadData[0].Results[0].laps - loadData[0].Results[i].laps) + " Giro" : "+" + (loadData[0].Results[0].laps - loadData[0].Results[i].laps) + " Giri") : ""}
+                          ${loadData[0].Results[i].status == "Retired" ? "DNF" : ""}
+                          ${loadData[0].Results[i].status == "Did not start" ? "DNS" : ""}
+                          ${loadData[0].Results[i].status == "Disqualified" ? "DSQ" : ""}
+                        </span>
+                      </p>
+                      `;
+    }
+  }
+}
+
+standingBtn.addEventListener("click", loadStandingPage);
+resultBtn.addEventListener("click", loadResultPage);
+
+loadStandingPage();
+// loadResultPage();
